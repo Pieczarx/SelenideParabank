06341a1932fd9435563ff7085566e6f3cbf5307b
Bartosz <bartek.pietrzyk46@gmail.com>
2023-11-14 18:11:32 +0100

    dodanie ValidTransferFundsTestTransferFunds

--- src/test/java/parabank/pages/TransferFundsPage.java
+++ src/test/java/parabank/pages/TransferFundsPage.java
@@ -1,17 +1,19 @@
 package parabank.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import parabank.config.TestMain;
 
 import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TransferFundsPage extends TestMain {
+public class TransferFundsPage extends Page {
     public SelenideElement transferFundsLink = $(By.linkText("Transfer Funds"));
-    public SelenideElement amountInput = $x("input[id='amount']");
-    public SelenideElement transferInput = $x("input[type='submit']");
+    public SelenideElement amountInput = $(By.cssSelector("input[id='amount']"));
+    public SelenideElement transferInput = $(By.cssSelector("input[type='submit']"));
     public SelenideElement errorMessage = $(By.id("amount.errors"));
 
     public SelenideElement firstAccount() {
@@ -30,12 +32,16 @@ public class TransferFundsPage extends TestMain {
     }
     public TransferFundsPage transfer(){
        transferFundsLink.click();
+       sleep(2000);
        amountInput.clear();
        String amount = "100";
-       $("#amountofCash").setValue(amount);
+       amountInput.setValue(amount);
        selectAccounts();
-       sleep(2000);
        transferInput.click();
-        return this;
+       sleep(2000);
+       return this;
+    }
+    public void completedTransferMessageShouldBeDisplayed() {
+        Assert.assertEquals(pageTitle.shouldBe(Condition.visible).getText(), "Transfer Complete!");
     }
 }
--- src/test/java/parabank/tests/TransferFunds/ValidTransferFundsTest.java
+++ src/test/java/parabank/tests/TransferFunds/ValidTransferFundsTest.java
@@ -7,7 +7,7 @@ import parabank.pages.LoginPage;
 import parabank.pages.Pages;
 import org.testng.annotations.BeforeClass;
 
-public class ValidTransferFundsTest {
+public class ValidTransferFundsTest extends TestMain{
     private Users standardUser;
     private void setUpTestUsers(){
         standardUser = new Users("firstName", "lastName","address","city","state","zipCode", "phone","ssn","username","password","password");
@@ -19,16 +19,18 @@ public class ValidTransferFundsTest {
         Pages.loginPage.login(username, password);
         Pages.loginPage.loggedMessageShouldBeDisplayed();
     }
+
     @Override
     @BeforeClass(alwaysRun = true)
     public void beforeClass() throws Exception{
         super.beforeClass();
         setup();
     }
+
     @Test(description = "TC01: Prawidłowy przelew środków")
     public void validTransferFundsTest() {
         Pages.transferFundsPage.transfer();
-
+        Pages.transferFundsPage.completedTransferMessageShouldBeDisplayed();
     }
 
 }
